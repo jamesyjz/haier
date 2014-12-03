b0e79e5dbaac49520d8a6b043bcc834225eed341
James Yang <james.yang@nurun.com>
2014-12-03 20:03:33 +0800

    1长屏幕滚一下到具体位置

--- images/j-mobile.js
+++ images/j-mobile.js
@@ -16,170 +16,42 @@ $(function(){
     $('#j-mask').show();
   }
 
-  $(window).scrollTop(0);
-  var nowPage =0;
-  var maxPage = 6;
-  var isFirst = true;
-  var isScroll = false;
-  var hammertime = new Hammer($('.j-main-wrap')[0]);
-  hammertime.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
-  hammertime.on('swipe', function(ev) {
-    //if(isScroll) return false;
-    var dir = ev.direction;
-    if(dir == 8){
-      switch (nowPage){
-        case 0 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap2').offset().top
-          }, 500, function(){
-            $('#j-godown').fadeOut(500);
-          });
-          nowPage++;
-          break;
-
-        case 1 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap3').offset().top
-          }, 500, function(){
-            
-          });
-          nowPage++;
-          break;
-
-        case 2 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap4').offset().top
-          }, 500, function(){
-            
-          });
-          nowPage++;
-          break;
-
-        case 3 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap5').offset().top
-          }, 500, function(){
-            
-          });
-          nowPage++;
-          break;
-
-        case 4 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap6').offset().top
-          }, 500, function(){
-            
-          });
-          nowPage++;
-          break;
-
-        case 5 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap7').offset().top
-          }, 500, function(){
-            
-          });
-          nowPage++;
-          break;
-      }
-    }else{
-      switch (nowPage){
-        case 6 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap6').offset().top
-          }, 500, function(){
-           
-          });
-          nowPage--;
-          break;
-
-        case 5 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap5').offset().top
-          }, 500, function(){
-           
-          });
-          nowPage--;
-          break;
-        case 4 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap4').offset().top
-          }, 500, function(){
-           
-          });
-          nowPage--;
-          break;
-        case 3 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap3').offset().top
-          }, 500, function(){
-           
-          });
-          nowPage--;
-          break;
-        case 2 : 
-          $('html, body').animate({
-            'scrollTop' : $('.j-hr-wrap2').offset().top
-          }, 500, function(){
-           
-          });
-          nowPage--;
-          break;
-        case 1 : 
-          $('html, body').animate({
-            'scrollTop' : 0
-          }, 500, function(){
-           
-          });
-          nowPage--;
-          break;
-      }
-    }
-    
-  });
-
-  
   $('.hr-logo, .con1-title, .hr-download').addClass('animated');
 
-  $('.j-hr-wrap2').waypoint(function(){
-    $('.con2-products, .con2-shopping-cart, .j-hr-wrap2 .common-hand').addClass('animated');
-  }, {offset: 150});
-
-  $('.j-hr-wrap3').waypoint(function(){
-    $('.con3-gift, .j-hr-wrap3 .common-hand').addClass('animated');
-  }, {offset: 150});
-
-  $('.j-hr-wrap4').waypoint(function(){
-    $('.con4-dianbingxiang, .con4-car, .con4-round, .j-hr-wrap4 .common-hand').addClass('animated');
-  }, {offset: 150});
-
-  $('.j-hr-wrap5').waypoint(function(){
-    $('.con5-dianbingxiang, .con5-money, .con5-gift, .con5-round, .j-hr-wrap5 .common-hand').addClass('animated');
-  }, {offset: 150});
-
-  $('.j-hr-wrap6').waypoint(function(){
-    $('.con6-diandengpao, .con6-jiaoliu, .con6-shuju, .con6-wenhao, .j-hr-wrap6 .common-hand').addClass('animated');
-  }, {offset: 150});
-
-  $('.j-hr-wrap7').waypoint(function(){
-    $('.con7-leftmobile, .con7-rightmobile, .j-hr-wrap7 .common-hand').addClass('animated');
-  }, {offset: 150});
-
-
-
-  //GA
-  $('#j-main-wrap .btn-dld').bind('click', function(){
-    _smq.push(['custom','海尔生活家APP推广','Haier-立即下载button']);
+  var mySwiper = new Swiper('.swiper-container',{
+    //Your options here:
+    mode:'vertical',
+    //etc..
+    onSlideChangeStart : function(index) {
+      //Do something when you touch the slide
+      if($('.j-hr-wrap1').hasClass('swiper-slide-visible')){
+        $('#j-godown').fadeIn(1000);
+      }else{
+        $('#j-godown').fadeOut(1000);
+      }
+      if($('.j-hr-wrap2').hasClass('swiper-slide-visible')){
+        $('.con2-products, .con2-shopping-cart, .j-hr-wrap2 .common-hand').addClass('animated');
+      }
+      if($('.j-hr-wrap3').hasClass('swiper-slide-visible')){
+        $('.con3-gift, .j-hr-wrap3 .common-hand').addClass('animated');
+      }
+      if($('.j-hr-wrap4').hasClass('swiper-slide-visible')){
+        $('.con4-dianbingxiang, .con4-car, .con4-round, .j-hr-wrap4 .common-hand').addClass('animated');
+      }
+      if($('.j-hr-wrap5').hasClass('swiper-slide-visible')){
+        $('.con5-dianbingxiang, .con5-money, .con5-gift, .con5-round, .j-hr-wrap5 .common-hand').addClass('animated');
+      }
+      if($('.j-hr-wrap6').hasClass('swiper-slide-visible')){
+        $('.con6-diandengpao, .con6-jiaoliu, .con6-shuju, .con6-wenhao, .j-hr-wrap6 .common-hand').addClass('animated');
+      }
+      if($('.j-hr-wrap7').hasClass('swiper-slide-visible')){
+        $('.con7-leftmobile, .con7-rightmobile, .j-hr-wrap7 .common-hand').addClass('animated');
+      }
+    }
   });
 
   $('#j-godown').bind('click', function(){
-    $('html, body').animate({
-      'scrollTop' : $('.j-hr-wrap2').offset().top
-    }, 500, function(){
-      $('#j-godown').fadeOut(500);
-    });
-    nowPage = 1;
-    _smq.push(['custom','海尔生活家APP推广','Haier-进入下一页箭头']);
+    mySwiper.swipeNext();
   })
 
 
---
+++ js/j-mobile.js
@@ -0,0 +1,72 @@
+$(function(){
+  var $elems = $('#j-main-wrap > section');
+  var winheight = $(window).height();
+  var fullheight = $(document).height();
+  var IPHONEURL = "https://itunes.apple.com/cn/app/yi-xin-mian-fei-liao-tian/id683688634?mt=8";
+  var ANDROIDURL = "http://yixin.dl.126.net/update/installer/yixin.apk";
+  var UA = navigator.userAgent.toLowerCase();
+
+  if(UA.indexOf('iphone') != -1) {
+    $('#j-main-wrap .btn-dld').attr('href', IPHONEURL);
+  }else{
+    $('#j-main-wrap .btn-dld').attr('href', ANDROIDURL);
+  }
+
+  if(UA.indexOf('iphone') != -1 && UA.indexOf('micromessenger') != -1) {
+    $('#j-mask').show();
+  }
+  
+  $('.hr-logo, .con1-title, .hr-download').addClass('animated');
+
+  $('.j-hr-wrap2').waypoint(function(){
+    $('.con2-products, .con2-shopping-cart, .j-hr-wrap2 .common-hand').addClass('animated');
+  }, {offset: 150});
+
+  $('.j-hr-wrap3').waypoint(function(){
+    $('.con3-gift, .j-hr-wrap3 .common-hand').addClass('animated');
+  }, {offset: 150});
+
+  $('.j-hr-wrap4').waypoint(function(){
+    $('.con4-dianbingxiang, .con4-car, .con4-round, .j-hr-wrap4 .common-hand').addClass('animated');
+  }, {offset: 150});
+
+  $('.j-hr-wrap5').waypoint(function(){
+    $('.con5-dianbingxiang, .con5-money, .con5-gift, .con5-round, .j-hr-wrap5 .common-hand').addClass('animated');
+  }, {offset: 150});
+
+  $('.j-hr-wrap6').waypoint(function(){
+    $('.con6-diandengpao, .con6-jiaoliu, .con6-shuju, .con6-wenhao, .j-hr-wrap6 .common-hand').addClass('animated');
+  }, {offset: 150});
+
+  $('.j-hr-wrap7').waypoint(function(){
+    $('.con7-leftmobile, .con7-rightmobile, .j-hr-wrap7 .common-hand').addClass('animated');
+  }, {offset: 150});
+
+  $(window).bind('touchmove', function(){
+    if(($(window).scrollTop()) > 0){
+      $('#j-godown').fadeOut(1000);
+    }else{
+      $('#j-godown').fadeIn(1000);
+    }
+  });
+
+  // $(window).bind('scroll', function(){
+  //   for(var i=0; i<$elems.length;i++) {
+  //      if ($(window).scrollTop() > winheight * i + (winheight / 2) ){
+  //         $elems.eq(i).children('.common-hand').addClass('re-animated');
+  //      }  
+  //   };
+  // })
+  
+
+ // $("#j-main-wrap").onepage_scroll({
+ //        sectionContainer: "section",
+ //        loop: true
+ //    });
+  
+  //GA
+  $('#j-main-wrap .btn-dld').bind('click', function(){
+    _smq.push(['custom','海尔生活家APP推广','Haier-立即下载button']);
+  });
+
+});
